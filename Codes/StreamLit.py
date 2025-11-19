a1ee9a4a0b909705baa87a2f885f08f23a6f296d
chain2z <69501192+chain2z@users.noreply.github.com>
2025-11-19 13:23:34 +0700

    Update StreamLit.py

--- Codes/StreamLit.py
+++ Codes/StreamLit.py
@@ -4,6 +4,9 @@ import streamlit as st
 import datetime as dt
 import pages as pages
 
+st.set_page_config(layout="wide")
+
+st.markdown("<p style=\"font-size: 70px; font-weight: bold;\">DSDE Project</p>",unsafe_allow_html=True)
 
 edaTab, overviewTab, authorsTab , publishersTab , subjectAreaTab,countryTab,collaborationTab,aiOverviewTab, aiTab,researchQuestionTab= st.tabs(["EDA","Overview" , "Authors" , "Publishers","Subject Areas", "Countries", "Collaboration","AI Overview","AI", "Research Question"])
 
